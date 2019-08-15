2ba1b726220a1b60be52a705ddede45c72d63bd3
Himanshu Patel <ptlhimanshu827@gmail.com>
2019-08-14 22:33:24 -0400

    Register : Reducer Completed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
@@ -20,6 +21,12 @@ class Register extends Component {
       this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps){
+      if(nextProps.errors) {
+        this.setState({errors: nextProps.errors});
+      }
+  }
+
   onChange(e){
     this.setState({[e.target.name]: e.target.value})
   }
@@ -33,19 +40,14 @@ class Register extends Component {
       password : this.state.password,
       password2: this.state.password2
     }
-    // axios.post('/api/users/register', newUser)
-    //   .then(res => console.log(res.data))
-    //   .catch(err => this.setState({errors: err.response.data}));
 
-    this.props.registerUser(newUser);
+    this.props.registerUser(newUser, this.props.history);
   }
   render () {
     const {errors} = this.state;
 
-    const { user } = this.props.auth;
     return(
       <div className="register">
-        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -89,10 +91,13 @@ class Register extends Component {
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 });
-export default connect(mapStateToProps, { registerUser})(Register);
+
+export default connect(mapStateToProps, { registerUser})(withRouter(Register));
